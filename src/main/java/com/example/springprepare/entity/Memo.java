7c8051bb580f26bf405f9bae8bf297ae14946b3c
liebigbear <jtw10155@naver.com>
2025-07-17 22:50:13 +0900

    refactor: 기존 JDBC -> JPA로 변경

--- src/main/java/com/example/springprepare/entity/Memo.java
+++ src/main/java/com/example/springprepare/entity/Memo.java
@@ -2,16 +2,23 @@ package com.example.springprepare.entity;
 
 
 import com.example.springprepare.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "memo")
 @NoArgsConstructor
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "username", nullable = false)
     private String username;
+    @Column(name = "contents", nullable = false)
     private String contents;
 
     public Memo(MemoRequestDto memoRequestDto) {
