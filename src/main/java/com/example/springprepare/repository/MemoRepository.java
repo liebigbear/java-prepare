aee2ee0fb3a2fd25d1e6211df41cae2c4037f12e
liebigbear <jtw10155@naver.com>
2025-07-19 09:05:36 +0900

    fix: update 안되는 오류 수정

---
+++ src/main/java/com/example/springprepare/repository/MemoRepository.java
@@ -0,0 +1,10 @@
+package com.example.springprepare.repository;
+
+import com.example.springprepare.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
+
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+}
