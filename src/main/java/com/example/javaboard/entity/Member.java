d52a44a474f25c04ce6d190a5e5b94ef62b6a3c8
liebigbear <jtw10155@naver.com>
2025-07-19 11:23:21 +0900

    feat: 회원가입 기능 구현(POST)

---
+++ src/main/java/com/example/javaboard/entity/Member.java
@@ -0,0 +1,32 @@
+package com.example.javaboard.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Integer age;
+
+    @Builder
+    public Member(String username, String password, Integer age) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+    }
+}
